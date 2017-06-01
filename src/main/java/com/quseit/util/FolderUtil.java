02a85c86161893805649641a5f24805e048c4795
hm <bugreject@gmail.com>
2017-06-01 18:04:14 +0800

    Improve editor, Fix some UI problem

--- src/main/java/com/quseit/util/FolderUtil.java
+++ src/main/java/com/quseit/util/FolderUtil.java
@@ -31,4 +31,28 @@ public class FolderUtil {
             return ret;
         }
     };
+
+    public static final Comparator<File> sortByName = new Comparator<File>() {
+        @Override
+        public int compare(File o1, File o2) {
+            String arg0 = o1.getName();
+            String arg1 = o2.getName();
+            String ext;
+            String ext2;
+            int ret;
+
+            try {
+                ext = arg0.substring(arg0.lastIndexOf(".") + 1, arg0.length()).toLowerCase();
+                ext2 = arg1.substring(arg1.lastIndexOf(".") + 1, arg1.length()).toLowerCase();
+
+            } catch (IndexOutOfBoundsException e) {
+                return 0;
+            }
+            ret = ext.compareTo(ext2);
+
+            if (ret == 0)
+                return arg0.toLowerCase().compareTo(arg1.toLowerCase());
+            return ret;
+        }
+    };
 }
