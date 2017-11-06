f36db6e0807041a76742438ea9926e9b34583557
hm <bugreject@gmail.com>
2017-11-06 17:46:29 +0800

    UPDATE sort scripts by name UPDATE Lib project icons

--- src/main/java/com/quseit/util/FolderUtil.java
+++ src/main/java/com/quseit/util/FolderUtil.java
@@ -3,10 +3,6 @@ package com.quseit.util;
 import java.io.File;
 import java.util.Comparator;
 
-/**
- * Created by Hmei on 2017-05-11.
- */
-
 public class FolderUtil {
     public static final Comparator<File> sortTypeByName = new Comparator<File>() {
         @Override
@@ -20,7 +16,6 @@ public class FolderUtil {
             try {
                 ext = arg0.substring(arg0.lastIndexOf(".") + 1, arg0.length()).toLowerCase();
                 ext2 = arg1.substring(arg1.lastIndexOf(".") + 1, arg1.length()).toLowerCase();
-
             } catch (IndexOutOfBoundsException e) {
                 return 0;
             }
