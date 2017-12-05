e40b5250dec7997db8c776c210ce9a067b8cb960
hm <bugreject@gmail.com>
2017-12-05 08:59:10 +0800

    FIX homepage rotate crash

---
+++ src/main/java/com/quseit/receiver/Bus.java
@@ -0,0 +1,12 @@
+package com.quseit.receiver;
+
+
+import org.greenrobot.eventbus.EventBus;
+
+public class Bus {
+    private static final EventBus INSTANCE = new EventBus();
+
+    public static EventBus getDefault() {
+        return INSTANCE;
+    }
+}
